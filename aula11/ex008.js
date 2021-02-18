cc66e24725cff7c419aa614d551f5813373027e2
Patrick Piccini <patrickbpiccini@hotmail.com>
2021-02-17 23:54:37 -0300

    Contições parte 1

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 80.5
+console.log(`a velocidade do seu carro é de ${vel}Km/h`)
+
+if(vel > 60){ // condição simples
+    console.log("Voce foi multado!")
+}
+
+console.log('Diriga com segurança')
