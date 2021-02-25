62ffba93a0b5a5cf6af37f1caaa0450b8432612d
Patrick Piccini <patrickbpiccini@hotmail.com>
2021-02-25 00:59:36 -0300

    Esse foi o ultima aula do Curso de JavaScript do canal 'Curso em Video', porem, irei aprofundar em mais algumas funcionalidades da linguagem, e tambem fazer exercicios para praticar a logica e adquirir mais conhecimento sobre o assunto :)

---
+++ aula17/objeto.js
@@ -0,0 +1,13 @@
+let amigo = {
+    nome: 'patrick',
+    sexo: 'M',
+    peso: 71.5 ,
+    engordar(p=0){
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
+amigo.engordar(8)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
